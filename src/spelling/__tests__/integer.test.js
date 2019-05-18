cc559074db7935888d474024e7921a2770a1ac6b
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-05-18 22:11:32 +0400

    changed folder structure for tests

--- src/spelling/__tests__/integer.test.js
+++ src/spelling/__tests__/integer.test.js
@@ -1,47 +1,47 @@
-const spellNumber = require('./index');
-const digits = require('./utils/translations').digits;
+const spellInteger = require('../integer');
+const digits = require('../../utils/translations').digits;
 
 describe('Digits from 0 to 9', function() {
   for (let i = 0; i < digits.length; i++) {
     it('correctly spells digit ' + i, function() {
-      const spelling = spellNumber(i);
+      const spelling = spellInteger(i);
       expect(spelling).toEqual(digits[i]);
     });
   }
   it('correctly spells negative digits', function() {
-    const negativeOne = spellNumber(-1);
+    const negativeOne = spellInteger(-1);
     expect(negativeOne).toEqual('mənfi bir');
-    const negativeFive = spellNumber(-5);
+    const negativeFive = spellInteger(-5);
     expect(negativeFive).toEqual('mənfi beş');
   });
 });
 
 describe('Numbers from 10 to 100', function() {
   it('correctly spells numbers from 10 to 100', function() {
-    const eleven = spellNumber(11);
+    const eleven = spellInteger(11);
     expect(eleven).toEqual('on bir');
-    const twentyThree = spellNumber(23);
+    const twentyThree = spellInteger(23);
     expect(twentyThree).toEqual('iyirmi üç');
-    const nintyNine = spellNumber(99);
+    const nintyNine = spellInteger(99);
     expect(nintyNine).toEqual('doxsan doqquz');
-    const eightySix = spellNumber(86);
+    const eightySix = spellInteger(86);
     expect(eightySix).toEqual('səksən altı');
-    const sixtyNine = spellNumber(69);
+    const sixtyNine = spellInteger(69);
     expect(sixtyNine).toEqual('altmış doqquz');
-    const thirtyOne = spellNumber(31);
+    const thirtyOne = spellInteger(31);
     expect(thirtyOne).toEqual('otuz bir');
-    const negativeThirtyOne = spellNumber(-31);
+    const negativeThirtyOne = spellInteger(-31);
     expect(negativeThirtyOne).toEqual('mənfi otuz bir');
   });
 });
 
 describe('Numbers from 100 to 1000', function() {
   it('correctly spells numbers from 100 to 1000', function() {
-    const hundredAndOne = spellNumber(101);
+    const hundredAndOne = spellInteger(101);
     expect(hundredAndOne).toEqual('yüz bir');
-    const nineHundredNintyNine = spellNumber(999);
+    const nineHundredNintyNine = spellInteger(999);
     expect(nineHundredNintyNine).toEqual('doqquz yüz doxsan doqquz');
-    const negativeSevenHundredThirtyEight = spellNumber(-738);
+    const negativeSevenHundredThirtyEight = spellInteger(-738);
     expect(negativeSevenHundredThirtyEight).toEqual(
       'mənfi yeddi yüz otuz səkkiz'
     );
@@ -50,17 +50,17 @@ describe('Numbers from 100 to 1000', function() {
 
 describe('Numbers from 1000 to 1000000', function() {
   it('correctly spells numbers from 1000 to 1000000', function() {
-    const thousandAndOne = spellNumber(1001);
+    const thousandAndOne = spellInteger(1001);
     expect(thousandAndOne).toEqual('min bir');
-    const thousandAndHundrenThirty = spellNumber(1130);
+    const thousandAndHundrenThirty = spellInteger(1130);
     expect(thousandAndHundrenThirty).toEqual('min yüz otuz');
-    const threeThousandFiveHundredSix = spellNumber(3506);
+    const threeThousandFiveHundredSix = spellInteger(3506);
     expect(threeThousandFiveHundredSix).toEqual('üç min beş yüz altı');
-    const negativeEightThousandThreeHundredAndThirtyOne = spellNumber(-80331);
+    const negativeEightThousandThreeHundredAndThirtyOne = spellInteger(-80331);
     expect(negativeEightThousandThreeHundredAndThirtyOne).toEqual(
       'mənfi səksən min üç yüz otuz bir'
     );
-    const bunchOfNines = spellNumber(990999);
+    const bunchOfNines = spellInteger(990999);
     expect(bunchOfNines).toEqual(
       'doqquz yüz doxsan min doqquz yüz doxsan doqquz'
     );
