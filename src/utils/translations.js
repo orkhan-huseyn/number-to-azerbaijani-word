6d34e20cf5ac92ebde00cfe2237b39c53463f994
Orkhan Huseynli <o.huseynli@asan.gov.az>
2019-05-18 15:34:55 +0400

    working on decimals

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -30,6 +30,8 @@ module.exports.BILLION = 'milyard';
 
 module.exports.NEGATIVE = 'mənfi';
 
+module.exports.POINT = 'tam';
+
 module.exports.OVER_TEN = 'onda';
 module.exports.OVER_HUNDRED = 'yüzdə';
 module.exports.OVER_THOUSAND = 'mində';
