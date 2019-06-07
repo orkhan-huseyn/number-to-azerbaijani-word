99fc9da28e9892d207fd127c23a3bd6ffd0efda4
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2019-06-07 23:52:40 +0400

    working on new bundling

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'bundle.min.js'
   },
   module: {
     rules: [{ test: /\.js/, use: 'babel-loader' }]
